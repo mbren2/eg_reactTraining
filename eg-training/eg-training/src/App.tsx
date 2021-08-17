9a0dcfc7c77332e03c78dc7c3db61e0655811cb5
Mike Brennan <mbrennan@ihie.org>
2021-08-17 13:53:46 -0400

    committing changes for function to fetch foods

--- eg-training/eg-training/src/App.tsx
+++ eg-training/eg-training/src/App.tsx
@@ -1,3 +1,26 @@
-export function App(){
-    return <h1>Pantry Manager</h1>
+type Food = {
+    name: string;
+    quantity: number;
+}
+
+const foods: Food[] = [
+    {name:"carrot", quantity: 1},
+    {name: "potato", quantity: 5}
+];
+
+export function App() {
+    function renderFoods() {
+        return foods.map((food) => <li>{food.name}</li>);
+    }
+
+    return (
+        //Use this empty tag (opposed to using a div) to give these elements a parent element 
+        //Without the empty tag or div here, we get an error bc h1 and ul need a parent tag
+        <>
+            <h1>Pantry Manager</h1>
+            <ul>
+                { renderFoods()}
+            </ul>
+        </>
+    );
 }
