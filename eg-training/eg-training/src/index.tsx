88ee25de16aa86f2c6f81a94d5c809111a34e40e
Mike Brennan <mbrennan@ihie.org>
2021-08-17 12:22:29 -0400

    initial commit

---
+++ eg-training/eg-training/src/index.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { render } from "react-dom";
+import { App } from "./App";
+
+const headStyle = {color: "blue", marginBottom: 10 };
+
+// our first react component 
+
+//HTML                        vs                          JSX
+//class                                                 className
+//For                                                   htmlFor
+//inline styles in HTML are strings                     Inline styles are objects
+function Heading(props: any) {
+    return (<h1 className="head" style={headStyle}>
+        {props.children}
+    </h1>
+    );
+    //</h1>return <h1>{ props.children }</h1>;
+}
+
+//render(<<Heading>Hello</Heading>>,document.getElementById("root"));
+render(<App />,document.getElementById("root"));
